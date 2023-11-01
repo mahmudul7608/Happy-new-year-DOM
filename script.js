499167033266e872a416c3b5554d6e065bf6f5f6
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-01 12:22:37 +0600

    Happy new year count down

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+  const currentTime = new Date();
+  const newYear = new Date(currentTime.getFullYear() + 1, 0, 1);
+  const timeRemaining = newYear - currentTime;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = String(days).padStart(2, '0');
+  document.getElementById("hours").textContent = String(hours).padStart(2, '0');
+  document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
+  document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
